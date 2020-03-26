ec2f88d85a2442ca6e00ec243feb29468c0871fd
Amber Williams <awilliams1234@live.com>
2020-03-26 19:57:19 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     student = []
-    https://learn-co-curriculum.github.io/student-scraper-test-page/index.html
+    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
   end
 
   def self.scrape_profile_page(profile_url)
