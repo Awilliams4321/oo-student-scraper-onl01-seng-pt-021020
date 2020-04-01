7d5cc394a02fe078328a786fe895dd81be55f67c
Amber Williams <awilliams1234@live.com>
2020-04-01 19:31:24 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,15 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
     student = []
     html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    doc = Nokogiri::HTML
   end
+
+
 
   def self.scrape_profile_page(profile_url)
 
